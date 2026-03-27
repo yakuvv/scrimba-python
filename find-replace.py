ac4b5b2f120cd29924bb6ae82f536a85fe37876a
yakuvv <angelynflorescarias24@gmail.com>
2026-03-27 21:54:28 +0800

    Add find and replace examples for string manipulation

---
+++ find-replace.py
@@ -0,0 +1,8 @@
+msg = 'Welcome to Python 101: Strings'
+print(msg.find('Python'))
+print(msg.replace('Python', 'Java'))
+
+msg1 = msg.replace('Python', 'ruby')
+print(msg1)
+
+print('Python' not in msg1)
---
+++ format.py
@@ -0,0 +1,6 @@
+name = 'JOHN'
+color = 'BLUE'
+msg = '[' + name.capitalize()+ '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name.capitalize()}] loves the color {color.lower()}'
+print(msg)
+print(msg1)
